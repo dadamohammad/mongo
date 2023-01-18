7e302387a17285b50fd750fae30a0b684e283cd7
dadamohammad <mmasomi155@gmail.com>
2023-01-18 16:15:57 +0330

    l

--- app/Http/Controllers/Api/v1/LoginController.php
+++ app/Http/Controllers/Api/v1/LoginController.php
@@ -4,64 +4,47 @@ namespace App\Http\Controllers\Api\v1;
 
 use App\Http\Controllers\Controller;
 use App\Models\Code;
+use App\Models\User;
 use Dotenv\Validator;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Str;
 
 class LoginController extends Controller
 {
+    /**
+     * Verify login
+     *
+     * @param Request $request
+     * @return string
+     */
     public function login(Request $request)
     {
-        $account = $request->account;
-
-//        $usernameValid = $request->validate([
-//            'username' => 'required',
-//            'password' => 'required'
-//        ]);
-//
-//        $phoneValid = $request->validate([
-//            'phone' => 'required',
-//        ]);
-//
-//        $emailValid = $request->validate([
-//            'email' => 'required',
-//        ]);
-
-        Validator::validate($request->account, [
-            'account' => 'r',
-        ]);
-
-
-
-        $randCode = rand('1000', '9999');
-
-        $code = Code::create([
-            'mobile' => $phone,
-            'rand_code' => $randCode
-        ]);
-
-        $code->save();
-        Http::post('https://rest.payamak-panel.com/api/SendSMS/SendSMS', [
-            'username' => '09122710574',
-            'password' => 'b4cac3e4-a242-4606-ab12-e7e9de96851a',
-            'from'     => '50004001710574',
-            'to'       => $phone,
-            'text'     => $randCode
-        ]);
-
-        return "کد به $phone ارسال شد";
-
-//        if (Auth::attempt($credentials)){
-//            return "وارد شد ";
-//        }
-//
-//        return back()->withErrors([
-//           'phone' => 'the provided credentials do not match our records.'
-//        ])->onlyInput('phone');
+        if ($this->getUser($request->account)->has_password) {
+            return 'has password';
+        }
+        $this->sendSms($request->phone);
+        return 'password sent';
     }
 
-    public function loginverify(Request $request)
+    /**
+     * Get user account type: username || email || password
+     *
+     * @param string $account
+     * @return string
+     */
+    protected function getAccountType(string $account): string
+    {
+        if (Str::contains($account, '@')) {
+            return 'email';
+        } elseif (preg_match('/[A-Za-z]/', $account)) {
+            return 'username' ;
+        }
+        return 'mobile';
+    }
+
+    public function loginVerify(Request $request)
     {
         \Illuminate\Support\Facades\Validator::validate($request->all(), [
             'phone' => 'required|unique: users,phone|size: 11',
@@ -81,11 +64,42 @@ class LoginController extends Controller
             $codeSendTime = $code->created_at;
             $diff = $currentTime->diff($codeSendTime)->i;
 
-
             if ($diff < 2) {
                 return 'کد با موفقیت ثبت شد';
             }
         }
         return "not create";
     }
+
+    /**
+     * Get user
+     *
+     * @param string $account
+     * @return mixed
+     */
+    protected function getUser(string $account): mixed
+    {
+        $type = $this->getAccountType($account);
+        return User::where($type, $account)->first();
+    }
+
+    /**
+     * Send sms to specific mobile number
+     *
+     * @param string $phone
+     * @return void
+     */
+    public function sendSms(string $phone): void
+    {
+        $randCode = rand('1000', '9999');
+        Http::post('https://rest.payamak-panel.com/api/SendSMS/SendSMS', [
+            'username' => '09122710574',
+            'password' => 'b4cac3e4-a242-4606-ab12-e7e9de96851a',
+            'from'     => '50004001710574',
+            'to'       => $phone,
+            'text'     => $randCode
+        ]);
+    }
+
+
 }
